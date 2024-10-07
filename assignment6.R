e6fd85bab339ab6ed80ed5b471ccfa72ddb3dea7
SAINATH9030224 <guddetir@usf.edu>
2024-10-06 22:35:26 -0400

    Add files via upload

---
+++ assignment6.R
@@ -0,0 +1,27 @@
+A=matrix(c(2,0,1,3), ncol=2)
+B=matrix(c(5,2,4,-1), ncol=2)
+
+A+B
+
+A-B
+
+
+
+
+diagonal_matrix <- diag(c(4, 1, 2, 3))
+
+print(diagonal_matrix)
+
+
+
+
+
+
+
+
+
+
+matrix<-diag(3,5,5)
+matrix[,1]<-c(3,2,2,2,2)
+matrix[1,]<-c(3,1,1,1,1)
+print(matrix)
